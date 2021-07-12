e9469b0d844e09305d7916dff8c9e920c22a2511
Aryan-Tanwar <85589795+Aryan-Tanwar@users.noreply.github.com>
2021-07-12 23:57:49 +0530

    Update main4.py

--- main4.py
+++ main4.py
@@ -12,7 +12,7 @@ def main():
         teamp = st.text_input("Type password for round 1 ")
         if teamp == "T1":
             st.subheader("Your Personality")
-            st.write("**Vijay Malya**")
+            st.write("**Vijay Maly**")
         else:
             st.write("try again incorrect password")
 
